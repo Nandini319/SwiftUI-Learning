1457229473969afa5b7dfad681dfbc018c7b25a7
Nandini Vithlani <nandinivithlani99@gmail.com>
2023-08-23 16:04:18 +0530

    Initial Commit

---
+++ SwiftUI Learning/SwiftUI_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LearningApp.swift
+//  SwiftUI Learning
+//
+//  Created by Nandini Vithlani on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
