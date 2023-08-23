c47d388698f567227a53528b082c0ca138097a31
Nandini Vithlani <nandinivithlani99@gmail.com>
2023-08-23 16:25:21 +0530

    Geometry Reader - SwiftUI

--- SwiftUI Learning/ContentView.swift
+++ SwiftUI Learning/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isCardExpanded = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                Text("Responsive Card")
+                    .font(.title)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .shadow(radius: 5)
+                    .frame(
+                        width: isCardExpanded ? geometry.size.width : geometry.size.width * 0.8,
+                        height: isCardExpanded ? geometry.size.height : 200 // Fixed height when not expanded
+                    )
+                    .position(
+                        x: geometry.size.width / 2,
+                        y: isCardExpanded ? geometry.size.height / 2 : geometry.size.height * 0.3
+                    )
+                    .onTapGesture {
+                        withAnimation {
+                            isCardExpanded.toggle()
+                        }
+                    }
+                
+                Spacer()
+            }
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
@@ -24,3 +46,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
